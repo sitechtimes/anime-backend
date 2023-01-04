9c8c4c609db057954932db403045b51feae75355
J0hns0n-Wang <johnsonwang529@gmail.com>
2023-01-04 09:31:05 -0500

    pkdspc

--- socialLogin/views.py
+++ socialLogin/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from dj_rest_auth.registration.views import SocialLoginView
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
