ed597f6a49f83e603123ab9eea270a3bf018df46
J0hns0n-Wang <johnsonwang529@gmail.com>
2023-01-09 10:16:03 -0500

    implemented check cookie, needs to be more polished

--- animeBackend/settings.py
+++ animeBackend/settings.py
@@ -86,7 +86,7 @@ SITE_ID = 1
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=10),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True, # IMPORTANT
     'BLACKLIST_AFTER_ROTATION': True, # IMPORTANT
