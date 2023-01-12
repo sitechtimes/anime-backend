6770eeb15adc0f8730c3f3c50125a8ab15bd4f5e
J0hns0n-Wang <johnsonwang529@gmail.com>
2023-01-12 17:06:53 -0500

    works

--- animeBackend/settings.py
+++ animeBackend/settings.py
@@ -193,3 +193,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+UTH_USER_MODULE = 'users.CustomUser'
+
--- users/models.py
+++ users/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from anime.models import Anime
 from django.contrib.auth.models import AbstractUser
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 class UserAnime(models.Model):
     anime = models.ForeignKey(Anime, on_delete=models.CASCADE)
     currently_watching = models.BooleanField()
@@ -26,3 +29,10 @@ class UserProfile(models.Model):
     user_anime = models.ManyToManyField( UserAnime,related_name='taken', blank=True)
     # profile_img = models.ImageField()     # need to add a media root for it to work(just search it)
 
+
+@receiver(post_save, sender=CustomUser)
+def create_user_customer(sender, instance, created, **kwargs):
+    if created:
+        user = UserProfile.objects.get_or_create(user=instance, grade=12)
+
+post_save.connect(create_user_customer, sender=CustomUser)
