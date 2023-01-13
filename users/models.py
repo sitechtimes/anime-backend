a6b4555986a8b76328096883b800a6d2d616ab41
J0hns0n-Wang <johnsonwang529@gmail.com>
2023-01-13 13:24:24 -0500

    sdcsc

--- users/models.py
+++ users/models.py
@@ -5,12 +5,20 @@ from django.contrib.auth.models import AbstractUser, User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+watching_status = [
+    ("NOT_WATCHING", "Not Watching"),
+    ("CURRENTLY_WATCHING",  "Currently watching"),
+    ("WATCHLIST", "Watchlist"),
+    ("FINISHED_ANIME", "Finished Anime")
+]
+
 class UserAnime(models.Model):
     anime = models.ForeignKey(Anime, on_delete=models.CASCADE)
-    currently_watching = models.BooleanField()
-    watchlist = models.BooleanField()
-    finished_anime = models.BooleanField()
+    # currently_watching = models.BooleanField()
+    # watchlist = models.BooleanField()
+    # finished_anime = models.BooleanField()
     rating = models.IntegerField(null=True)
+    watching_status = models.CharField(max_length=20, choices=watching_status, default="NOT_WATCHING")
 
     # def __str__(self):
     #     return self.rating
@@ -25,7 +33,7 @@ class CustomUser(AbstractUser):
 
 class UserProfile(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    grade = models.IntegerField()
+    # grade = models.IntegerField()
     user_anime = models.ManyToManyField( UserAnime,related_name='taken', blank=True)
     # profile_img = models.ImageField()     # need to add a media root for it to work(just search it)
 
@@ -39,14 +47,14 @@ def create_user_customer(sender, instance, created, **kwargs):
             print(index)
             account = instance.email[index + 1:]
             print(account)
-            if account == ("nycstudents.net" or "schools.nyc.gov"):
+            if account != ("nycstudents.net" or "schools.nyc.gov"):
                 user = User.objects.get(email = instance.email)
                 print(user)
                 print("wassup")
                 # user.remove()
                 return 
             else:
-                user = UserProfile.objects.get_or_create(user=instance, grade=12)
+                user = UserProfile.objects.get_or_create(user=instance)
         except Exception:
             return
 
