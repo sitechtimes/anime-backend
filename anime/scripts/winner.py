e5bd1e4a5f30d9bf247de1cca0b272a8c4117644
J0hns0n-Wang <johnsonwang529@gmail.com>
2023-02-07 12:39:00 -0500

    sdc

---
+++ anime/scripts/winner.py
@@ -0,0 +1,19 @@
+from anime.models import Anime, AnimeAwards, Awards
+
+anime_awards = [
+    "Best Anime",
+    "Best Character Design",
+    "Best Animation",
+    "Best New Series",
+    "Best Continuing Series",
+    "Best Ending Sequence",
+    "Best Opening Sequence",
+    "Best Main Character",
+    "Best Supporting Character",
+    "Best Action",
+    "Best Comedy",
+    "Best Drama",
+    "Best Fantasy",
+    "Best Romance",
+    "Best Anime Song"
+]
