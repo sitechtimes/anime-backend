37b94407136395c69cd0a014d641663c996d8aee
J0hns0n-Wang <johnsonwang529@gmail.com>
2023-02-13 18:20:56 -0500

    sd

--- anime/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import AnimeAwards
-
-class WinnerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AnimeAwards
-        fields = [
-            ""
-        ]
