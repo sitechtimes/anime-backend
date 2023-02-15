92fe8ec6589fcc9391e769df669ed76ac0bbd358
harveyj123 <71397990+harveyj123@users.noreply.github.com>
2023-02-15 10:59:29 -0500

    as

--- anime/tests.py
+++ anime/tests.py
@@ -1,7 +1,44 @@
-# from django.test import TestCase
-# from .models import Genre, Awards, AnimeAwards, Anime
-# import datetime
+from django.test import TestCase
+from .models import Genre, Anime, AnimeAwards, Awards
+from datetime import date
 # # Create your tests here.
+
+
+class test_genreModel(TestCase):
+    def setUp(self):
+        self.date = date.today()
+        self.anime_awards = []
+        self.all_winners = []
+
+    def test_genreCreation(self):
+        all_awards = Awards.objects.all()
+        
+        for award in all_awards:
+            self.anime_awards.append(award)
+
+        for anime_award in self.anime_awards:
+            all_anime_awards = AnimeAwards.objects.filter(award__award_name = anime_award)
+            highest_vote_count = max(all_anime_awards, key=lambda y: y.vote_count).vote_count
+            print(highest_vote_count)
+                
+            filtered_anime_awards = all_anime_awards.filter(vote_count = highest_vote_count)
+            print(len(filtered_anime_awards))
+            for filtered_anime_award in filtered_anime_awards:
+                filtered_anime_name = filtered_anime_award.anime.anime_name
+                filtered_award_name = filtered_anime_award.award.award_name
+                filtered_anime = Anime.objects.get(anime_name = filtered_anime_name)
+                filtered_award = Awards.objects.get(award_name = filtered_award_name)
+                filtered_award.date = self.date
+                filtered_award.save()
+                filtered_anime.anime_awards.add(filtered_award)
+                filtered_anime.save()
+                self.all_winners.append(filtered_anime_award)
+     
+        self.assertEqual(self.all_winners, self.all_winners)
+
+   
+
+
 
 
 # class test_genreModel(TestCase):
