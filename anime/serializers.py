9224602518731c0d5e4baed9a3d94b042b0b6e2f
J0hns0n-Wang <johnsonwang529@gmail.com>
2023-03-02 11:37:03 -0500

    sdc

--- anime/serializers.py
+++ anime/serializers.py
@@ -21,22 +21,23 @@ class AnimeSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Anime
-        fields = [
-            "mal_id",
-            "anime_name",
-            "media_type",
-            "image_url",
-            "small_image_url",
-            "large_image_url",
-            "trailer_youtube_url",
-            "episodes",
-            "status",
-            "aired_from",
-            "aired_to",
-            "summary",
-            "anime_studio",
-            "anime_genre",
-            "anime_awards",
-            "number_rating"
-        ]
+        fields = "__all__"
+        # fields = [
+        #     "mal_id",
+        #     "anime_name",
+        #     "media_type",
+        #     "image_url",
+        #     "small_image_url",
+        #     "large_image_url",
+        #     "trailer_youtube_url",
+        #     "episodes",
+        #     "status",
+        #     "aired_from",
+        #     "aired_to",
+        #     "summary",
+        #     "anime_studio",
+        #     "anime_genre",
+        #     "anime_awards",
+        #     "number_rating"
+        # ]
     
