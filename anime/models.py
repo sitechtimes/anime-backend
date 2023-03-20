857393b17a5680e8c209d0c86a4dd698d5c341a7
harveyj123 <71397990+harveyj123@users.noreply.github.com>
2023-03-19 22:41:14 -0400

    as

--- anime/models.py
+++ anime/models.py
@@ -19,6 +19,7 @@ class Studio(models.Model):
     def __str__(self):
         return self.studio
 
+
 class Awards(models.Model):
     award_name = models.CharField(max_length=255)
     # award_img = models.ImageField() #search for more parameters
@@ -26,6 +27,7 @@ class Awards(models.Model):
 
     def __str__(self):
         return self.award_name
+
 
 class Character(models.Model):
     mal_id = models.IntegerField(null=True)
@@ -50,26 +52,32 @@ class Anime(models.Model):
     status = models.CharField(max_length=255, null=True)
     aired_from = models.DateField(null=True)
     aired_to = models.DateField(null=True)
-    season = models.CharField(max_length=255,null=True)
+    season = models.CharField(max_length=255, null=True)
     summary = models.TextField(null=True)
     anime_studio = models.ManyToManyField(Studio)
     anime_genre = models.ManyToManyField(Genre)
     anime_awards = models.ManyToManyField(Awards)
-    number_rating = models.IntegerField(null=True, default=0)#number of ratings
+    number_rating = models.IntegerField(
+        null=True, default=0)  # number of ratings
+    avg_rating = models.DecimalField(
+        max_digits=5, decimal_places=2,  default=0)
 
     def __str__(self):
         return self.anime_name
 
+
 class AnimeAwards(models.Model):
     vote_count = models.IntegerField(default=0)
-    anime = models.ForeignKey(Anime, on_delete=models.CASCADE, blank=True, null=True)
-    award = models.ForeignKey(Awards, on_delete=models.CASCADE, blank=True, null=True)
+    anime = models.ForeignKey(
+        Anime, on_delete=models.CASCADE, blank=True, null=True)
+    award = models.ForeignKey(
+        Awards, on_delete=models.CASCADE, blank=True, null=True)
     allUsers = models.ManyToManyField("users.UserProfile")
-    
+
     def __str__(self):
         return f"{self.anime.anime_name}, {self.award.award_name}"
-    
- 
+
+
 # class Vote(models.Model):
 #     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 #     anime = models.ForeignKey(Anime, on_delete=models.CASCADE)
@@ -78,6 +86,6 @@ class AnimeAwards(models.Model):
 
 class AllWinners(models.Model):
     winner = models.ForeignKey(AnimeAwards, on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return f"{self.winner.anime.anime_name}, {self.winner.award.award_name}"
