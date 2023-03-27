776ce70f1ca16ba444af12f863af8cee8ac6e9ef
Harvey Jiang <harveyjiang11@gmail.com>
2023-03-27 16:06:35 -0400

    ewf

--- animeBackend/settings.py
+++ animeBackend/settings.py
@@ -26,7 +26,7 @@ load_dotenv()
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['anime.siths.dev', '.localhost', '127.0.0.1', '[::1]']
 
