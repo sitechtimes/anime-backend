fff9d69be5a6539028a93559387bba966474166b
Johnson Wang <johnsonwang529@gmail.com>
2023-03-28 22:26:21 -0400

    sdf

--- anime/views.py
+++ anime/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework import permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from anime.models import Anime
@@ -7,6 +8,7 @@ from anime.serializers import AnimeSerializer
 
 # Create your views here.
 class AnimeView(APIView):
+    permission_classes = (permissions.AllowAny,)
     def get(self, request, format = None):
         animes = Anime.objects.all()
         serializer = AnimeSerializer(animes, many = True)
