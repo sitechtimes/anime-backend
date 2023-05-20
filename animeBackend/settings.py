953ad2e7e9ba7b35430ff1ce79213d4f909de5d0
Johnson Wang <johnsonwang529@gmail.com>
2023-05-20 15:58:04 -0400

    Revert "sd"
    This reverts commit 67fe8d57a30def36cddad5bc28e9642356b2c2f6.

--- animeBackend/settings.py
+++ animeBackend/settings.py
@@ -159,13 +159,9 @@ WSGI_APPLICATION = 'animeBackend.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    # "default": dj_database_url.parse(
-    #         os.environ.get("DB")
-    #     )
+    "default": dj_database_url.parse(
+            os.environ.get("DB")
+        )
 }
 
 
